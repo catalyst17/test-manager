0ea2ddb1a6910ad32f101d66df8f0deefb52a34a
Arsenii <ar.poryadin@yandex.ru>
2021-05-04 17:28:16 +0300

    ability to run sample containers in the current process or in a celery task

--- tasks.py
+++ tasks.py
@@ -1,5 +1,9 @@
 from .celery import app
+import docker
+
+docker_client = docker.from_env()
 
 @app.task
-def add(x, y):
-    return x + y
+def echo_from_buster():
+    out = docker_client.containers.run("python:3.8-slim-buster", 'ls -la root', auto_remove=True)
+    return out.decode('utf-8')
