12c909e28b1e62b95338c1f107fff7edef15f22c
Arsenii <ar.poryadin@yandex.ru>
2021-05-09 23:55:46 +0300

    define main models of ccts

--- apps/ccts_app/apps.py
+++ apps/ccts_app/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CctsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ccts_app'
+    name = 'apps.ccts_app'
--- ccts/settings.py
+++ ccts/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ccts_app'
+    'apps.ccts_app'
 ]
 
 MIDDLEWARE = [
